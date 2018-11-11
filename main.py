c6cfa5334bcbee21b4146432c7b860832e09e5fd
austinsheep <sleepysoftware@gmail.com>
2018-11-11 15:10:42 -0800

    Changed generation of points to ensure no duplicates.

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-visual = True    # Whether or not to generate visualization
-N = 20           # Number of generated points
-max_value = 5    # The max value of each axis
-color = 'r'      # The color of the points
-area = np.pi * 8 # The area of the points
-alpha = 0.75     # Alpha of the points
+visual = True     # Whether or not to generate visualization
+N = 20            # Number of generated points
+max_value = N * 2 # The max value of each axis
+color = 'r'       # The color of the points
+area = np.pi * 8  # The area of the points
+alpha = 0.75      # Alpha of the points
 
-points = max_value * np.random.rand(N, 2)
+# Generation of an array of random, unique points
+points = random.sample(range(max_value), max_value)
+points = np.reshape(points, (-1, 2))
 
 if visual:
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    fig, (axG, axQ) = plt.subplots(1, 2, sharey=True)
     x, y = zip(*points)
 
-    ax1.scatter(x, y, s=area, c=color, alpha=alpha)
-    ax1.set_title('Gift Wrapping')
-    ax1.set_xlabel('X')
-    ax1.set_ylabel('Y')
+    axG.scatter(x, y, s=area, c=color, alpha=alpha)
+    axG.set_title('Gift Wrapping')
+    axG.set_xlabel('X')
+    axG.set_ylabel('Y')
 
-    ax2.scatter(x, y, s=area, c=color, alpha=alpha)
-    ax2.set_title('Quick Hull')
-    ax2.set_xlabel('X')
+    axQ.scatter(x, y, s=area, c=color, alpha=alpha)
+    axQ.set_title('Quick Hull')
+    axQ.set_xlabel('X')
 
     plt.show()
