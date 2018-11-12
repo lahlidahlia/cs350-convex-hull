b84a1542580c82b7ab81aab329cbf8cb05d9ea0d
austinsheep <sleepysoftware@gmail.com>
2018-11-12 11:43:16 -0800

    Switched points datatype from list of tuples to 2D np.ndarray.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Convex Hull solver using NumPy
+### (Work In Progress)
+
+To use visualization, pass the `-v` flag when running `main.py`.
--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-import random
+from sys import argv
 import numpy as np
 import matplotlib.pyplot as plt
 
-visual = True     # Whether or not to generate visualization
-N = 20            # Number of generated points
-max_value = N * 2 # The max value of each axis
-color = 'r'       # The color of the points
-area = np.pi * 8  # The area of the points
-alpha = 0.75      # Alpha of the points
+# `gen_rand_points` takes in `N`, the number of points to be generated.
+# An array of random, unique points is returned.
+# (Returned array could include three or more collinear points)
+def gen_rand_points(N):
+    x = np.arange(N)
+    y = np.arange(N)
+    np.random.shuffle(x)
+    np.random.shuffle(y)
 
-# Generation of an array of random, unique points
-points = random.sample(range(max_value), max_value)
-points = np.reshape(points, (-1, 2))
+    return np.column_stack((x, y))
 
-if visual:
+# N = 10
+points = gen_rand_points(10)
+
+# Properties of the points
+color = 'r'
+area  = 20
+alpha = 0.75
+
+if '-v' in argv: # Create visualization
     fig, (axG, axQ) = plt.subplots(1, 2, sharey=True)
-    x, y = zip(*points)
+    x, y = points.T
 
+    # Gift Wrapping algorithm plot
     axG.scatter(x, y, s=area, c=color, alpha=alpha)
     axG.set_title('Gift Wrapping')
     axG.set_xlabel('X')
     axG.set_ylabel('Y')
 
+    # Quick Hull algorithm plot
     axQ.scatter(x, y, s=area, c=color, alpha=alpha)
     axQ.set_title('Quick Hull')
     axQ.set_xlabel('X')
