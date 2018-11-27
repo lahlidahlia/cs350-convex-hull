73c7acd519113ef916406d7376f5bce4586f0637
austinsheep <sleepysoftware@gmail.com>
2018-11-27 14:54:47 -0800

    Refined benchmarking. The mean timing for each algorithm is saved to the CSV file. Benchmarking only runs when visualization does not in order to not view the multiple reruns of each algorithm.

--- main.py
+++ main.py
@@ -1,60 +1,86 @@
 import time
 import config
+import random
+import statistics
 import datagen as dg
 import matplotlib.pyplot as plt
 from sys import argv
 from pprint import pprint
+from scipy.spatial import ConvexHull
 
 def run_dataset(dataset, function, sizes):
-    with open(config.timings_file + '.csv', 'a') as results_file:
-        results_file.write("%s" % dataset)
+    '''
+    Runs the given dataset on the list of input sizes and the
+    corresponding algorithms to run on those.
+    '''
+    if not config.visual:
+        with open(config.timings_file + '.csv', 'a') as results_file:
+            results_file.write("%s" % dataset)
 
     plot = None # Scatter plot
     for size, algos in sizes:
-        points = function(size)
         if config.visual:
+            points = function(size)
             # config.p_area = size somethin'..
             x, y = points.T
-            plot = config.ax.scatter(x, y, s=config.p_area, c=config.p_color,
-                    alpha=config.p_alpha)
-        run_algorithms(dataset, algos, size, points)
-        if config.visual:
-            #plot.remove()
-            pass
+            plot = config.ax.scatter(x, y, s=config.p_area,
+                    c=config.p_color, alpha=config.p_alpha)
+            run_algorithms(dataset, algos, size, points)
+            plot.remove()
+        else:
+            with open(config.timings_file + '.csv', 'a') as results_file:
+                results_file.write(",%u," % size)
+            times = {a: [] for a in algos}
+            for i in range(10): # How many times to run each dataset
+                points = function(size)
+                timings = run_algorithms(dataset, algos, size, points)
+                for name, time in timings.items():
+                    times[name].append(time)
 
-def run_algorithms(dataset_name, algos, input_size, points):
-    with open(config.timings_file + '.csv', 'a') as results_file:
-        results_file.write(",%u," % input_size)
+            for algo, l_time in times.items():
+                name, fun = config.algorithms.get(algo)
+                print('ALGO:\t'    + name)
+                print('MEDIAN:\t', statistics.median(l_time))
+                print('MEAN:\t',   statistics.mean(l_time))
+                print('STDEV:\t',  statistics.stdev(l_time))
+                with open('results.csv', 'a') as results_file:
+                    results_file.write("%s,%s\n"
+                            % (name,statistics.mean(l_time)))
 
-    first = True
-    for algo in algos:
+def run_algorithms(dataset, algos, input_size, points):
+    '''
+    Runs the given algorithm on the provided input points. Returns a
+    dictionary for each algorithm and its end time.
+    '''
+    times = {a: -1 for a in algos}
+    while algos:
+        algo = random.choice(algos)
+        algos = algos.replace(algo, "")
         algo_name, function = config.algorithms.get(algo)
         config.image_path = 'frames/%s_%s_%s.png' \
-                % (dataset_name, algo_name, str(input_size))
+                % (dataset, algo_name, str(input_size))
 
         print("Solving with " + algo_name + " Algorithm")
         if config.visual:
             if config.lines:
                 config.lines.pop(0).remove()
             config.timer.set_text("0.00 sec")
-            config.ax.set_title("Dataset: %s\nAlgorithm: %s\nData Size:%u" %
-                    (dataset_name, algo_name, input_size))
+            config.ax.set_title(
+                    "Dataset: %s\nAlgorithm: %s\nData Size: %u" %
+                    (dataset, algo_name, input_size))
         start_time = time.time()
         results = function(points, start_time)
         end_time = time.time() - start_time
+        times[algo] = end_time * 1000 # Sec to mSec
+
         pprint(results)
         print("Solved in %.2f seconds\n" % end_time)
 
-        output_size = len(results)
-        if not first:
-            with open(config.timings_file + '.csv', 'a') as results_file:
-                results_file.write(",,")
-        else:
-            first = False
+        # Compare the results to SciPy's
+        scipy_results = points[ConvexHull(points).vertices].tolist()
+        assert all(i in results for i in scipy_results)
 
-        with open('results.csv', 'a') as results_file:
-            results_file.write("%u,%s,%f\n"
-                    % (output_size, algo_name, end_time*1000))
+    return times
 
 if __name__ == '__main__':
     if '-v' in argv or '--visual' in argv:
@@ -65,13 +91,14 @@ if __name__ == '__main__':
         config.ax.set_xlabel('X')
         config.ax.set_ylabel('Y')
         config.visual = True
-        config.timer = config.ax.text(0.9, 0.95, "", ha='center', va='center',
+        config.timer = config.ax.text(0.9, 0.95, "",
+                ha='center', va='center',
                 transform = config.ax.transAxes)
-   
-    # Write the first row of the CSV file with titles
-    with open(config.timings_file + '.csv', 'w') as results_file:
-        results_file.write('Dataset,Input Size,Output Size,Algorithm,'
-        + 'Timing (ms),Worst case number of operations,Operations/ms\n')
+    else:
+        # Write the first row of the CSV file with titles
+        with open(config.timings_file + '.csv', 'w') as results_file:
+            results_file.write(
+                    'Dataset,Input Size,Algorithm,Mean Timing (ms)\n')
 
     # Run the following datasets:
     # Each dataset has a list of sizes with
@@ -83,8 +110,8 @@ if __name__ == '__main__':
         config.ax.set_xlim([-0.1, 1.1])
         config.ax.set_ylim([-0.1, 1.1])
     run_dataset('Random', dg.gen_random_data, [
-        #[10,    'BGQ'],
-        [100,   'M'],
+        [10,    'BGQ'],
+        #[100,   'QM'],
         #[1000,  'M'],
         #[10000, 'Q']
     ])
